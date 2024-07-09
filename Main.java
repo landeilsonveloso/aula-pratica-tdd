009815e43c02e740d3cfd2196bcb2d2f85564858
landeilsonveloso <landeilsonveloso@gmail.com>
2024-07-09 12:27:08 -0300

    ajustes

--- Main.java
+++ Main.java
@@ -1,11 +1,59 @@
 class Dollar {
-    int amount = 10;
-    Dollar(int amount) {}			
-    void times(int multiplier) {}
- }	
+    private amount;
+
+    Dollar(int amount) {
+       this.amount= amount;
+    }
+
+    Dollar times(int multiplier) {
+       return new Dollar(amount * multiplier);
+    }
+ }
 
  public void testMultiplication() {
     Dollar five = new Dollar(5);
-    five.times(2);
-    assertEquals(10, five.amount);
+    assertEquals(new Dollar(10), five.times(2));
+    assertEquals(new Dollar(15), five.times(3));
  }
+
+ public void testEquality() {
+    assertTrue(new Dollar(5).equals(new Dollar(5)));
+    assertFalse(new Dollar(5).equals(new Dollar(6)));
+    assertTrue(new Franc(5).equals(new Franc(5)));
+    assertFalse(new Franc(5).equals(new Franc(6)));
+ }
+
+ public boolean equals(Object object)  {
+    Dollar dollar = (Dollar) object;
+    return amount == dollar.amount;
+ }
+
+ public void testFrancMultiplication() {
+    Franc five = new Franc(5);
+    assertEquals(new Franc(10), five.times(2));
+    assertEquals(new Franc(15), five.times(3));
+ }
+ 
+ class Franc {   
+    private int amount;					
+    Franc(int amount) {      
+       this.amount= amount;
+     }					
+     Franc times(int multiplier)  {      
+        return new Franc(amount * multiplier);					
+     }   
+     public boolean equals(Object object) {					
+        Franc franc = (Franc) object;      
+        return amount == franc.amount;					
+      }					
+ }
+
+ class Money  {
+    protected int amount;
+    
+    public boolean equals(Object object)  {
+       Money money = (Money) object;
+       return amount == money.amount;
+    }   
+ }
+
