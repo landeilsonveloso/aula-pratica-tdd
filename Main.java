9ab04056f3bb3445d94e4abeb55b10035e60d77d
landeilsonveloso <landeilsonveloso@gmail.com>
2024-07-09 12:18:22 -0300

    first commit

---
+++ Main.java
@@ -0,0 +1,7 @@
+echo "# aula-pratica-tdd" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/landeilsonveloso/aula-pratica-tdd.git
+git push -u origin main
